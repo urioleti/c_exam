b562eb28dfcdc25bfa735e541007a0974f98558f
root <root@DESKTOP-B3KROIU.localdomain>
2021-12-09 17:30:34 +0100

    added maff_revalpha.c

---
+++ lvl0/maff_revalpha.c
@@ -0,0 +1,28 @@
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(char even, char odd)
+{
+	int	l;
+
+	even = 'Y';
+	odd = 'z';
+	l = 26;
+	while (l > 0)
+	{
+		ft_putchar(odd);
+		odd--;
+		odd--;
+		l--;
+		ft_putchar(even);
+		even--;
+		even--;
+		l--;
+	}
+	ft_putchar('\n');
+	return (0);
+}
