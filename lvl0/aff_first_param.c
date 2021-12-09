007a59a05677f45659a1610675bb826a44b290dc
root <root@DESKTOP-B3KROIU.localdomain>
2021-12-09 12:56:32 +0100

    Added aff_first_param.c

---
+++ lvl0/aff_first_param.c
@@ -0,0 +1,29 @@
+#include<unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+int	main(int argc, char **argv)
+{
+	char	*str;
+	int		i;
+
+	if (argc >= 2)
+	{
+		str = argv [1];
+		i = 0;
+		while (str[i] != '\0')
+		{
+			ft_putchar(str[i]);
+			i++;
+		}
+		ft_putchar('\n');
+	}
+	else
+	{
+		ft_putchar('\n');
+	}
+	return (0);
+}
